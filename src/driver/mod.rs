68bebbfbe29574b6ae83805e281ffff0eb56ebcb
Mara Schulke <mail@maras.cloud>
2025-03-15 15:30:20 +0100

    Handle obtaining and driver loop

--- src/driver/mod.rs
+++ src/driver/mod.rs
@@ -6,6 +6,9 @@ use std::{
     sync::LazyLock,
 };
 
+use crossbeam_channel::{bounded, unbounded, Receiver, Sender};
+use uuid::Uuid;
+
 use model::Model;
 
 pub mod model;
@@ -13,8 +16,6 @@ pub mod utils;
 
 static MODEL: LazyLock<model::Model> =
     LazyLock::new(|| Model::load("./resources").expect("loading the mode failed"));
-
-//struct Prompt<'m>(&'m Model);
 
 struct Context {
     handle: RefCell<Option<DriverHandle>>,
@@ -26,36 +27,90 @@ const CONTEXT: Context = Context {
 
 struct Driver {
     model: &'static Model,
+    prompts: Receiver<Prompt>,
+    tokens: Sender<Token>,
 }
 
 impl Driver {
-    fn run(self) {
-        todo!()
-    }
-
     /// Detaches the driver from the current thread
     pub fn detach() -> DriverHandle {
+        // NOTE:
+        // Allow at most 2 concurrent prompts, as we are not running on
+        // good hardware. This proves that we can do concurrent inference without allowing to
+        // exhaust resources on small machines.
+        //
+        // If one runs this outside of a research context this limitation should be dropped.
+        let (ptx, prx) = bounded::<Prompt>(2);
+        let (ttx, trx) = unbounded::<Token>();
+
+        let handle = DriverHandle {
+            prompt: ptx,
+            token: trx,
+        };
+
         std::thread::spawn(|| {
-            // HINT:
-            // this ensures we are hitting the lazy lock
-            // code and load the model into memory
-            let driver = Self { model: &MODEL };
+            let mut driver = Self {
+                // NOTE:
+                // This ensures we are hitting the lazy lock
+                // code and load the model into memory
+                model: &MODEL,
+                prompts: prx,
+                tokens: ttx,
+            };
 
             // Push the event loop
-            driver.run()
+            loop {
+                driver.push();
+            }
+
+            // Unset the handle to make it impossible to obtain new handles
+            CONTEXT.handle.replace(None);
         });
 
-        DriverHandle {}
+        CONTEXT.handle.replace(Some(handle.clone()));
+
+        handle
+    }
+
+    /// Push the drivers event loop
+    fn push(&mut self) {
+        todo!()
     }
 }
 
-struct DriverHandle {}
+struct Prompt {
+    id: Uuid,
+}
+
+enum Token {
+    Eof { prompt: Uuid },
+}
+
+#[derive(Clone)]
+struct DriverHandle {
+    prompt: Sender<Prompt>,
+    token: Receiver<Token>,
+}
 
 impl DriverHandle {
-    /// This obtains a handle to the current driver.
+    /// Obtain a handle to the current driver.
     ///
     /// If we are running in a context where there is no driver thread this function will panic.
     pub fn current() -> Self {
-        todo!();
+        CONTEXT
+            .handle
+            .borrow()
+            .clone()
+            .expect("No pgpt driver thread is running")
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Driver;
+
+    #[test]
+    fn setup() {
+        let handle = Driver::detach();
     }
 }
